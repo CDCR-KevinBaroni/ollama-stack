c1b9b8764db8a249df373020a46d89f684749ea9
Kevin Baroni <kevin.baroni@cdcr.ca.gov>
2026-05-12 15:32:44 -0700

    Added forground processing of stack to start script.

--- start_stack.sh
+++ start_stack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 STACK_NAME=ollama
-docker stack deploy -c ollama-stack.yml --prune $STACK_NAME
+docker stack deploy -c ollama-stack.yml --prune $STACK_NAME --detach=false 
 sleep 4s
 docker service ls
