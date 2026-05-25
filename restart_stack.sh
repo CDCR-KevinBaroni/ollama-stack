0c8e165458cfcd1267a5fb4c10fcabf0a1969d53
Kevin Baroni <kevin.baroni@cdcr.ca.gov>
2026-05-25 13:22:17 -0700

    Normalize line endings and improve formatting in stack management scripts.

--- restart_stack.sh
+++ restart_stack.sh
@@ -3,4 +3,3 @@ STACK_NAME=ollama
 ./stop_stack.sh
 sleep 10
 ./start_stack.sh
-
--- start_stack.sh
+++ start_stack.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 STACK_NAME=ollama
-docker stack deploy -c ollama-stack.yml --prune $STACK_NAME --detach=false 
+docker stack deploy -c ollama-stack.yml --prune $STACK_NAME --detach=false
 sleep 4s
 docker service ls
 echo -e "\nLogin to the OLLAMA Web UI at http://$(hostname -I | awk '{print $1}'):8080/"
+
