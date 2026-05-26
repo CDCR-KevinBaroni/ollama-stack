74a69e14d86405b41fc3f9355f38fb34931bcb49
Kevin Baroni <kevin.baroni@cdcr.ca.gov>
2026-05-25 17:05:22 -0700

    Enhance environment variables for ollama service and improve login message formatting in start_stack.sh

--- start_stack.sh
+++ start_stack.sh
@@ -3,5 +3,5 @@ STACK_NAME=ollama
 docker stack deploy -c ollama-stack.yml --prune $STACK_NAME --detach=false
 sleep 4s
 docker service ls
-echo -e "\nLogin to the OLLAMA Web UI at http://$(hostname -I | awk '{print $1}'):8080/"
+echo -e "\n\nLogin to the OLLAMA Web UI at http://$(hostname -I | awk '{print $1}'):8080/\n\n"
 
